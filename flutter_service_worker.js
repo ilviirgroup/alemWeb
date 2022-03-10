54345349f0eb732d07ecc0dbc1c4942ad4aaeff2
ilviirgroup@gmail.com <ilviirgroup@gmail.com>
2022-03-10 10:20:06 +0500

    api 96.30....

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "ec2faf3712afed1f35646ecf6d1c916c",
-"/": "ec2faf3712afed1f35646ecf6d1c916c",
-"main.dart.js": "fdf0ed3e2b7e59b648c15185f78c831c",
+"index.html": "a41c321c6cbe397dfa02814ea71d14f5",
+"/": "a41c321c6cbe397dfa02814ea71d14f5",
+"main.dart.js": "c1448ff47dc9859ed08688e04f0732c2",
 "manifest.json": "d48d7236818f6f92f1933989103bfd4a",
 "version.json": "cc546180c5f58647f89b1257ae3f4ac4"
 };
